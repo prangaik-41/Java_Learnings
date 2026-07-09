175079a69f198741e5099079f703d52a55991e39
Pranav Gaikwad <pranav.ctrl@gmail.com>
2026-07-10 00:41:25 +0530

    variables and Calculations

---
+++ Calculation_variables.java
@@ -0,0 +1,14 @@
+public class Calculation_variables {
+    public static void main(String[]args){
+
+        int a = 12;
+        int b = 20;
+        int sum = a+b;
+        int sub = a-b;
+        int multiply = a* b;
+
+        System.out.println("Addition:  "+ sum);
+        System.out.println("Substraction: "+ sub);
+        System.out.println("Multiply: "+ multiply);
+    }
+}
