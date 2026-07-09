f7671127d772e23714d23060261ab6e501746e52
Pranav Gaikwad <pranav.ctrl@gmail.com>
2026-07-10 00:12:00 +0530

    Right Sided stars

---
+++ Simple_right_stars.java
@@ -0,0 +1,10 @@
+public class Simple_right_stars {
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+    
+}
