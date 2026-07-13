7a6dbf1a0e9c1d93833bcc29bc761fc6b5070cdf
Pranav Gaikwad <pranav.ctrl@gmail.com>
2026-07-13 10:52:23 +0530

    Switch statement

---
+++ Switch.java
@@ -0,0 +1,19 @@
+public class Switch {
+    public static void main(String[]args){
+        int day = 1 ;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2 :
+                System.out.println("Tuesday");
+                break;
+            case 3 :
+                System.out.println("Wenesday");
+                break;
+
+        }
+
+    }
+    
+}
